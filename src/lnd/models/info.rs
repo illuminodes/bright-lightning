6bdf9be1759ef1ccf565f3dad7e7f6f5a874a136
DevPardx <devpardo@idesignsv.com>
2025-04-15 14:10:42 -0600

    Map LND Info struct with API

--- src/lnd/models/info.rs
+++ src/lnd/models/info.rs
@@ -1,11 +1,49 @@
 use serde::{Deserialize, Serialize};
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Chain {
+    pub chain: String,
+    pub network: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Feature {
+    pub name: String,
+    pub is_required: bool,
+    pub is_known: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FeaturesEntry {
+    pub key: u32,
+    pub value: Feature,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LndInfo {
-    identity_pubkey: String,
-    block_height: u32,
+    pub version: String,
+    pub commit_hash: String,
+    pub identity_pubkey: String,
+    pub alias: String,
+    pub color: String,
+    pub num_pending_channels: u32,
+    pub num_active_channels: u32,
+    pub num_inactive_channels: u32,
+    pub num_peers: u32,
+    pub block_height: u32,
+    pub block_hash: String,
+    pub best_header_timestamp: String,
+    pub synced_to_chain: bool,
+    pub synced_to_graph: bool,
+    pub testnet: bool,
+    pub chains: Vec<Chain>,
+    pub uris: Vec<String>,
+    pub features: HashMap<String, Feature>,
+    pub require_htlc_interceptor: bool,
+    pub store_final_htlc_resolutions: bool,
 }
+
 impl TryFrom<&String> for LndInfo {
     type Error = anyhow::Error;
     fn try_from(value: &String) -> Result<Self, Self::Error> {
